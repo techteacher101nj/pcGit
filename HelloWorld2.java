543a6f20a9e44c03b688a0e4adcff2e4701d5a43
Lisa Waddell <90416736+techteacher101nj@users.noreply.github.com>
2025-07-29 14:59:25 -0400

    hw created

---
+++ HelloWorld2.java
@@ -0,0 +1,6 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        // This line prints "Hello, World!" to the console
+        System.out.println("Hello, Lisa!");
+    }
+}
